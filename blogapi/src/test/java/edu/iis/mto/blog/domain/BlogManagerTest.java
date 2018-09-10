5d9a9aed68688bc7bf9d63da322a5816f02ce88e
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-09-10 15:41:18 +0200

    dodano setUp dla klasy testowej BlogManagerTest

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,7 +1,11 @@
 package edu.iis.mto.blog.domain;
 
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -25,11 +29,43 @@ public class BlogManagerTest {
     @MockBean
     UserRepository userRepository;
 
+    @MockBean
+    BlogPostRepository blogPostRepository;
+
+    @MockBean
+    LikePostRepository likePostRepository;
+
     @Autowired
     DataMapper dataMapper;
 
     @Autowired
     BlogService blogService;
+
+    private User user, user2;
+    private BlogPost blogPost;
+
+    @Before
+    public void setUp() {
+
+        user = new User();
+        user.setFirstName("Tomasz");
+        user.setLastName("Bogdan");
+        user.setEmail("tobo@domain.com");
+        user.setAccountStatus(AccountStatus.NEW);
+        user.setId(11L);
+
+        user2 = new User();
+        user2.setFirstName("Janusz");
+        user2.setLastName("Toporek");
+        user2.setEmail("jantop@domain.com");
+        user2.setAccountStatus(AccountStatus.CONFIRMED);
+        user2.setId(32L);
+
+        blogPost = new BlogPost();
+        blogPost.setId(1L);
+        blogPost.setUser(user);
+        blogPost.setEntry("Test Enry");
+    }
 
     @Test
     public void creatingNewUserShouldSetAccountStatusToNEW() {
@@ -39,5 +75,7 @@ public class BlogManagerTest {
         User user = userParam.getValue();
         Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
     }
+
+
 
 }
